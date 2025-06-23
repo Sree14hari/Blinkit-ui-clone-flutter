e816543110fdf02e024446742fc746efc8349087
SREEHARI <sreehari14shr@gmail.com>
2025-06-23 18:18:07 +0530

    ongoing with cart page implementation

--- lib/ui/screens/shopingscreen/shoping_screen.dart
+++ lib/ui/screens/shopingscreen/shoping_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:blinkit_clone/ui/widgets/customappbar.dart';
 import 'package:flutter/material.dart';
 
@@ -52,6 +54,94 @@ class ShopingScreen extends StatelessWidget {
                 ),
               ),
             ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16, 0, 8, 6),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(10)),
+                        height: 108,
+                        width: 96,
+                        child: ClipRRect(
+                          borderRadius: BorderRadiusGeometry.circular(10),
+                          child: Image.asset(
+                            "assets/images/milk.png",
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 66, top: 96),
+                        child: SizedBox(
+                          height: 18,
+                          width: 30,
+                          child: TextButton(
+                            onPressed: () {},
+                            style: ButtonStyle(
+                              padding: WidgetStateProperty.all(EdgeInsets.zero),
+                              backgroundColor:
+                                  WidgetStateProperty.all(Colors.white),
+                              shape: WidgetStateProperty.all(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                              ),
+                              side: WidgetStateProperty.all(
+                                BorderSide(color: Colors.green, width: 2),
+                              ),
+                            ),
+                            child: Center(
+                              child: Text(
+                                "Add",
+                                style: TextStyle(
+                                    fontSize: 8,
+                                    color: Colors.green), // Smaller font
+                                overflow: TextOverflow.clip,
+                                softWrap: false,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 2,
+                  ),
+                  Text(
+                    "Amul Taaza Toned\nFresh Milk",
+                    style:
+                        TextStyle(fontFamily: "poppins regular", fontSize: 10),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Row(
+                    children: [
+                      Image.asset(
+                        "assets/images/stopwatch.png",
+                        height: 13,
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        "16 MINS",
+                        style: TextStyle(
+                            fontFamily: "poppinst regular",
+                            fontSize: 10,
+                            color: Colors.grey),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
           ],
         ));
   }
