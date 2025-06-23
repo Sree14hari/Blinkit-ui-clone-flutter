27d6631882820d19e55f206f9962071a263547ea
SREEHARI <sreehari14shr@gmail.com>
2025-06-23 21:41:34 +0530

    ui complete

--- lib/ui/screens/category/categorysection.dart
+++ lib/ui/screens/category/categorysection.dart
@@ -1,3 +1,5 @@
+import 'package:blinkit_clone/ui/widgets/customappbar.dart';
+import 'package:blinkit_clone/ui/widgets/minicards.dart';
 import 'package:flutter/material.dart';
 
 class Categorysection extends StatelessWidget {
@@ -6,8 +8,148 @@ class Categorysection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Category section"),
+      appBar: CustAppBar(
+        textColor: Colors.black,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16, 16, 0, 0),
+              child: Text("Grocery & Kitchen",
+                  style: TextStyle(fontFamily: "poppins bold", fontSize: 14)),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            SingleChildScrollView(
+                padding: EdgeInsets.fromLTRB(16, 0, 16, 8),
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  spacing: 15,
+                  children: [
+                    MiniCards(
+                      data: "Vegetables &\n      Fruits",
+                      url: "assets/images/veg/vegitables.png",
+                    ),
+                    MiniCards(
+                      data: "Atta, Dal &\n    Rice",
+                      url: "assets/images/veg/flour.png",
+                    ),
+                    MiniCards(
+                      data: "Oil, Ghee &\n   Masala",
+                      url: "assets/images/veg/groc.png",
+                    ),
+                    MiniCards(
+                      data: "Dairy, Bread &\n       Milk",
+                      url: "assets/images/veg/milkshakes.png",
+                    ),
+                    MiniCards(
+                      data: "Vegetables &\n  Fruits",
+                      url: "assets/images/veg/biscuits.png",
+                    ),
+                  ],
+                )),
+            SizedBox(
+              height: 10,
+            ),
+            SingleChildScrollView(
+                padding: EdgeInsets.fromLTRB(16, 0, 16, 8),
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  spacing: 15,
+                  children: [
+                    MiniCards(
+                      data: "Dry Fruits &\n   Cereals",
+                      url: "assets/images/veg/kellogs.png",
+                    ),
+                    MiniCards(
+                      data: "Ice Creams &\n  much more",
+                      url: "assets/images/veg/icecream.png",
+                    ),
+                    MiniCards(
+                      data: "Kitchen &\nAppliances",
+                      url: "assets/images/veg/mixer.png",
+                    ),
+                    MiniCards(
+                      data: "Tea &\nCoffees",
+                      url: "assets/images/veg/coffee.png",
+                    ),
+                    MiniCards(
+                      data: "Noodles &\nPacket Food",
+                      url: "assets/images/veg/maggie.png",
+                    ),
+                  ],
+                )),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16, 16, 0, 8),
+              child: Text("Snacks & Drinks",
+                  style: TextStyle(fontFamily: "poppins bold", fontSize: 14)),
+            ),
+            SingleChildScrollView(
+                padding: EdgeInsets.fromLTRB(16, 0, 16, 10),
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  spacing: 15,
+                  children: [
+                    MiniCards(
+                      data: "Chips &\nNamkeens",
+                      url: "assets/images/snacks/chips.png",
+                    ),
+                    MiniCards(
+                      data: "Sweets &\nChocalates",
+                      url: "assets/images/snacks/sweets.png",
+                    ),
+                    MiniCards(
+                      data: "Drinks &\nJuices",
+                      url: "assets/images/snacks/drinks.png",
+                    ),
+                    MiniCards(
+                      data: "Sauces &\nSpreads",
+                      url: "assets/images/snacks/sauce.png",
+                    ),
+                    MiniCards(
+                      data: "Beauty &\nCosmetics",
+                      url: "assets/images/snacks/cosmets.png",
+                    ),
+                  ],
+                )),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16, 16, 0, 8),
+              child: Text("Household Essentials",
+                  style: TextStyle(fontFamily: "poppins bold", fontSize: 14)),
+            ),
+            SingleChildScrollView(
+                padding: EdgeInsets.fromLTRB(16, 0, 16, 10),
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  spacing: 15,
+                  children: [
+                    MiniCards(
+                      data: "Detergents",
+                      url: "assets/images/snacks/surf.png",
+                    ),
+                    MiniCards(
+                      data: "Soaps",
+                      url: "assets/images/snacks/soap.png",
+                    ),
+                    MiniCards(
+                      data: "Oils",
+                      url: "assets/images/snacks/oil.png",
+                    ),
+                    MiniCards(
+                      data: "Sofas",
+                      url: "assets/images/snacks/sofa.png",
+                    ),
+                    MiniCards(
+                      data: "Perfumes",
+                      url: "assets/images/snacks/spray.png",
+                    ),
+                  ],
+                )),
+          ],
+        ),
       ),
     );
   }
--- lib/ui/screens/printer section/printerpage.dart
+++ lib/ui/screens/printer section/printerpage.dart
@@ -1,3 +1,4 @@
+import 'package:blinkit_clone/ui/widgets/customappbar.dart';
 import 'package:flutter/material.dart';
 
 class Printerpage extends StatelessWidget {
@@ -6,8 +7,114 @@ class Printerpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("printer page"),
+      backgroundColor: Color(0XFFFBF0CE),
+      appBar: CustAppBar(
+        textColor: Colors.black,
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 30,
+          ),
+          Center(
+            child: Column(
+              children: [
+                Text(
+                  "Print Store",
+                  style: TextStyle(fontFamily: "poppins bold", fontSize: 32),
+                ),
+                Text(
+                  "Blinkit ensures secure prints at every stage",
+                  style: TextStyle(
+                      fontFamily: "poppins bold",
+                      fontSize: 14,
+                      color: Color(0XFF9C9C9C)),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Container(
+                    height: 170,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Row(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Documents",
+                                style: TextStyle(
+                                  fontFamily: "poppins bold",
+                                  fontSize: 14,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                "⟡ Price starting at rs 3/page",
+                                style: TextStyle(
+                                  fontFamily: "poppins regular",
+                                  fontSize: 14,
+                                ),
+                              ),
+                              Text(
+                                "⟡ Paper quality: 70 GSM",
+                                style: TextStyle(
+                                  fontFamily: "poppins regular",
+                                  fontSize: 14,
+                                ),
+                              ),
+                              Text(
+                                "⟡ Single side prints",
+                                style: TextStyle(
+                                  fontFamily: "poppins regular",
+                                  fontSize: 14,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              TextButton(
+                                onPressed: () {},
+                                style: ButtonStyle(
+                                  backgroundColor:
+                                      WidgetStateProperty.all(Colors.green),
+                                  shape: WidgetStateProperty.all(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+                                  ),
+                                ),
+                                child: Text(
+                                  "Upload Files",
+                                  style: TextStyle(
+                                      fontFamily: "poppins bold",
+                                      fontSize: 14,
+                                      color: Colors.white),
+                                ),
+                              )
+                            ],
+                          ),
+                          Spacer(),
+                          Image.asset(
+                            "assets/images/snacks/docs.png",
+                            height: 90,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
